70ffb48af43cb78c67da2c82102c731d2b03d0ec
Reese <reesemiyowalther@gmail.com>
2025-07-25 06:59:17 -1000

    Initial Commit

---
+++ BirthdayApp/BirthdayAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdayAppApp.swift
+//  BirthdayApp
+//
+//  Created by rwalther28 on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct BirthdayAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
