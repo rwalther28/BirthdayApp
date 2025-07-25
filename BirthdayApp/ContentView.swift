f67495b597bc391e8b6004e997ebf95da607b11f
Reese <reesemiyowalther@gmail.com>
2025-07-25 09:17:32 -1000

    Update a friend

--- BirthdayApp/ContentView.swift
+++ BirthdayApp/ContentView.swift
@@ -13,6 +13,7 @@ struct ContentView: View {
     @Environment(\.modelContext) private var context
     @State private var newName = ""
     @State private var newBirthday = Date.now
+    @State private var selectedFriend: Friend?
     
     var body: some View {
         NavigationStack{
@@ -23,10 +24,18 @@ struct ContentView: View {
                         Spacer()
                         Text(friend.birthday, format: .dateTime.month(.wide).day().year())
                         }
+                    .onTapGesture {
+                        selectedFriend = friend
                     }
+                }
                     .onDelete(perform: deleteFriend)
             }
             .navigationTitle("Birthdays")
+            .sheet(item: $selectedFriend) { friend in
+                NavigationStack {
+                    EditFriendView(friend: friend)
+                }
+            }
             .safeAreaInset(edge: .bottom) {
                 VStack(alignment: .center, spacing: 20) {
                     Text("New Birthday")
